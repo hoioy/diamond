48610f9872bcc036f7f0d45d7cc25182dfdf5211
zhaozhao <zhaozhao@mail.taiji.com.cn>
2020-04-29 23:00:57 +0800

    Add: mobile 更改配置

---
+++ jiayin/jiayin-mobile/utils/request.js
@@ -0,0 +1,23 @@
+const websiteUrl = 'http://localhost:7779/';
+// const websiteUrlw = 'http://10.0.60.59:7779/';
+
+const request = function(data) {
+	if (uni.getStorageSync('token')) {
+		return uni.request({
+			url: websiteUrl + data.url,
+			method: data.method,
+			data: data.data,
+			header: {
+				'Authorization': 'Bearer ' + uni.getStorageSync('token').access_token
+			}
+		})
+	} else {
+		return uni.request({
+			url: websiteUrl + data.url,
+			method: data.method,
+			data: data.data
+		})
+	}
+}
+
+export default request
