25856dc352d9796b19ca047116e5165ae816010f
125536039@qq.com <125536039@qq.com>
2020-06-04 23:58:01 +0800

    日期格式化

---
+++ jiayin/jiayin-mobile/utils/date.js
@@ -0,0 +1,28 @@
+var dateFormat = {
+    padLeftZero: function (str) {
+        return ('00' + str).substr(str.length)
+    },
+    formatDate: function (date, fmt) {
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+        }
+        let o = {
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds()
+        }
+        for (let k in o) {
+            if (new RegExp(`(${k})`).test(fmt)) {
+                let str = o[k] + ''
+                fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? str : this.padLeftZero(str))
+            }
+        }
+        return fmt
+    }
+
+}
+
+
+export default dateFormat;
