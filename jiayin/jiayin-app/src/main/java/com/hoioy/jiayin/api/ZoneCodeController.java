6fd4de90179d2d170ba4053917d13e99186a27ef
125536039@qq.com <125536039@qq.com>
2020-06-26 19:42:27 +0800

    图标 path

---
+++ jiayin/jiayin-app/src/main/java/com/hoioy/jiayin/api/ZoneCodeController.java
@@ -0,0 +1,37 @@
+package com.hoioy.jiayin.api;
+
+import com.hoioy.diamond.common.api.BaseController;
+import com.hoioy.diamond.common.dto.PageDTO;
+import com.hoioy.diamond.common.dto.ResultDTO;
+import com.hoioy.diamond.common.exception.BaseException;
+import com.hoioy.jiayin.dto.MsgTypeDTO;
+import com.hoioy.jiayin.dto.ZoneCodeDTO;
+import com.hoioy.jiayin.service.IMsgTypeService;
+import com.hoioy.jiayin.service.IZoneCodeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "区划表接口")
+@RestController
+@RequestMapping("/jiayin/zonecode")
+public class ZoneCodeController extends BaseController<IZoneCodeService, ZoneCodeDTO> {
+
+    @ApiOperation(value = "分页", notes = "分页")
+    @PostMapping("/page")
+    public ResultDTO page(@RequestBody PageDTO<MsgTypeDTO> pageDTO) throws BaseException {
+        PageDTO page = iBaseService.getPage(pageDTO);
+        return new ResultDTO(page);
+    }
+
+    @ApiOperation(value = "根据parenId查询", notes = "根据parenId查询,parentId为null或者空字符串，则查询根节点")
+    @GetMapping("/findByParentId")
+    public ResultDTO findByParentId(@RequestParam("parentId") String parentId) throws BaseException {
+        List<MsgTypeDTO> msgTypeDTOs = iBaseService.findByParentId(parentId);
+        return new ResultDTO(msgTypeDTOs);
+    }
+
+}
+
