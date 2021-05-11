1e2681e1ef7cc1a287ae57db7ad6b5db760c9511
zhaozhao <zhaozhao@mail.taiji.com.cn>
2021-05-12 00:04:21 +0800

    Mod: jdk11启动失败的问题

--- diamond-app-backend-sample/src/main/java/com/hoioy/sample/conf/Swagger2Config.java
+++ diamond-app-backend-sample/src/main/java/com/hoioy/sample/conf/Swagger2Config.java
@@ -14,7 +14,6 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -82,8 +81,8 @@ public class Swagger2Config implements WebMvcConfigurer {
     }
 
     private List<SecurityReference> defaultAuth() {
-        AuthorizationScope[] authorizationScopes = (AuthorizationScope[]) Arrays.asList(
-                new AuthorizationScope("global", "accessEverything")).toArray();
-        return ListUtil.of(new SecurityReference(apiKeyName, authorizationScopes));
+        return ListUtil.of(new SecurityReference(apiKeyName, new AuthorizationScope[]{
+                new AuthorizationScope("global", "accessEverything")
+        }));
     }
 }
